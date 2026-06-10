4d3e91d190b7c624a19cb815b868fe0b5db88c2c
haulem7 <hayouayana@gmail.com>
2026-06-10 06:51:56 +0300

    frontend and backend connected

---
+++ packages/server/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenev from 'dotenv';
+
+
+dotenev.config();
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req: Request, res:Response ) => {
+    res.send(process.env.OPEN_API_KEY)
+});
+
+app.get('/api/hello', (req: Request, res:Response ) => {
+    res.json({ message: "Hello World!"});
+})
+
+app.listen(port, () => {
+    console.log(`Server host is running on http://localhost:${port}`)
+});
